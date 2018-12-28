feb39ed14996cfdae622d990b79b29dab9c2401c
Shuichiro Fukuoka <syumai@gmail.com>
2018-12-29 01:19:52 +0900

    Added multi reader

---
+++ io/multi.ts
@@ -0,0 +1,38 @@
+import { Reader, ReadResult } from 'deno';
+
+export class MultiReader {
+  private readers: Array<Reader>;
+  constructor(...readers: Array<Reader>) {
+    this.readers = [...readers];
+  }
+
+  async read(p: Uint8Array): Promise<ReadResult> {
+    while (this.readers.length > 0) {
+      // Optimization to flatten nested multiReaders
+      if (this.readers.length === 1) {
+        if (this.readers[0] instanceof MultiReader) {
+          const nestedMultiReader = this.readers[0] as MultiReader;
+          this.readers = nestedMultiReader.readers;
+        }
+      }
+      let { nread, eof } = await this.readers[0].read(p);
+      if (eof) {
+        this.readers.shift();
+      }
+      if (nread > 0 || !eof) {
+        if (eof && this.readers.length > 0) {
+          // Don't reaturn eof yet. More readers remain.
+          eof = false;
+        }
+        return {
+          nread,
+          eof,
+        };
+      }
+    }
+    return {
+      nread: 0,
+      eof: true,
+    };
+  }
+}
